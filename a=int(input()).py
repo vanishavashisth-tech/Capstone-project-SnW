2e8e5c0fcf80f51d61e3f6072646a4a227f73867
vanisha vashisth <vanishavashisth@vanishas-MacBook-Pro.local>
2025-11-20 23:56:00 +0530

    first code commit

---
+++ a=int(input()).py
@@ -0,0 +1,6 @@
+a=int(input())
+if (a==1):
+    print("Stay indoors")
+if (a==0):
+        print("Go for a walk")
+
---
+++ def greater(a,b):.py.py
@@ -0,0 +1,11 @@
+canvas{
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #00000033;
+}      
+    
+                          
+   
